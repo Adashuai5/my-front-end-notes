66122f6aee3c0c369395957039a6712b65f9d076
adashuai5 <ad873606339@gmail.com>
2019-02-15 20:53:45 +0800

    新增代码实现

---
+++ sort-demo/new.js
@@ -0,0 +1,13 @@
+// 实现一个 new
+function create() {
+    // 创建一个新对象
+    let obj = new Object()
+    // 获得构造函数
+    let Con = [].shift.call(arguments)
+    // 链接到原型
+    obj.__proto__ = Con.prototype
+    // 绑定 this，执行构造函数
+    let result = Con.apply(obj, arguments)
+    // 确保 new 出来的是个对象
+    return typeof obj === 'object' ? result : obj
+}
